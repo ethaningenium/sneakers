9ce7b998101387e32282eb6cfd1c563658ec585b
ethaningenium <143124094+ethaningenium@users.noreply.github.com>
2024-01-02 17:59:32 +0100

    first commit

---
+++ src/lib/contract.ts
@@ -0,0 +1,12 @@
+export type ProductType = {
+  id: number;
+  brand: string;
+  title: string;
+  description: string;
+  images: string[];
+  colors: string[];
+  sizes: number[];
+  price: number;
+  oldprice: number;
+  rating: number;
+};
---
+++ src/pages/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="bg-green-500 w-96 h-96"></div>;
+}
+
+export default App;
