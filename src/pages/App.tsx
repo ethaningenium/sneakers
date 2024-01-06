eb93a6306f80bc9d48e8abf3e06d39d26f1fc890
ethaningenium <143124094+ethaningenium@users.noreply.github.com>
2024-01-06 14:20:13 +0100

    card added

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,26 +1,22 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Card from "@/components/Card";
 import { useEffect, useState } from "react";
 import { ProductType } from "@/lib/contract";
 import { fetchAll } from "@/lib/fetch";
 
 function App() {
-  const [sneakers, setSneakers] = useState<ProductType[]>();
-  
+  const [items, setItems] = useState<ProductType[]>([]);
+
   useEffect(() => {
-    async function fetch() {
-      const elem = await fetchAll();
-      setSneakers(elem);
-    }
-    fetch();
+    fetchAll().then((res) => setItems(res));
   }, []);
   return (
     <>
       <Header />
-
-      <div className="w-full h-96 bg-zinc-100 flex justify-center items-center">
-        {sneakers?.map((elem) => {
-          return <span>{elem.title}</span>;
+      <div className="w-full bg-zinc-100 flex flex-wrap justify-center items-center p-12 gap-8">
+        {items.map((elem) => {
+          return <Card key={elem.id} {...elem} />;
         })}
       </div>
       <Footer />
