855ffae1794bfb73be3af426ff1c2217ace4f0ed
ethaningenium <143124094+ethaningenium@users.noreply.github.com>
2024-01-07 12:54:51 +0100

    bug #4 fixed

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Header />
-      <div className="w-full bg-zinc-100 flex flex-wrap justify-center items-center p-12 gap-8">
+      <div className="w-full bg-zinc-100 flex flex-wrap justify-center items-center p-12 gap-8 ">
         {data?.map((elem) => {
           return <Item key={elem.id} {...elem} />;
         })}
